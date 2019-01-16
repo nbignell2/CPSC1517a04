459d13deede9962067ef253c30b5dccb60d95f66
nbignell2 <46579584+nbignell2@users.noreply.github.com>
2019-01-16 14:17:38 -0700

    Create Turn.cs
    Attemped to create a class for players turns.

---
+++ CSReviewSolution/OOPsReview/Turn.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    public class Turn
+    {
+        public int Player1Roll { get; set; }
+        public int Player2Roll { get; set; }
+
+        public Turn()
+        {
+
+        }
+
+        public Turn(int player1, int player2)
+        {
+            Player1Roll = player1;
+            Player2Roll = player2;
+        }
+
+
+        public string RollResults()
+        {
+            //Call roll from seperate class??
+        }
+    }
+}
