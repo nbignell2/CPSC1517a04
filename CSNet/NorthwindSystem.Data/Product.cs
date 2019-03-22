96594c76d3655b87125831ca93b45c667731bbe9
nbignell2 <46579584+nbignell2@users.noreply.github.com>
2019-03-22 10:04:21 -0600

    class

---
+++ CSNet/NorthwindSystem.Data/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// the annotations used within the .Data project will require the 
+// System.ComponenetModel.DataAnnotation assembly
+// this assembly is added via your references
+
+
+namespace NorthwindSystem.Data
+{
+    //use an annotation to link this class to the appropriate
+    // SQL table
+    [Table("Products")]
+    public class Product
+    {
+    }
+}
--- CSNet/NorthwindSystem/DAL/NorthwindContext.cs
+++ CSNet/NorthwindSystem/DAL/NorthwindContext.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NorthwindSystem.Data
+namespace NorthwindSystem.DAL
 {
-    public class Class1
+    class NorthwindContext
     {
     }
 }
